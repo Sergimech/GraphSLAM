b61cd31a9c79af164e09f23e2689b98e6d35524a
David Swords <david@swords.computer>
2016-12-10 04:53:46 +0100

    minor progress made.

--- src/graph.h
+++ src/graph.h
@@ -28,26 +28,25 @@ using namespace Eigen;
 using namespace sensor_msgs;
 
 class Graph {
-
-	public:
-		vector<Node*> node_list;
-		vector<Edge*> edge_list;
-		Node * last_node;
-		//
-		Graph(double resolution, double range_threshold, double max_range, double min_range);
-		~Graph();
-		void addNode(GraphPose pose, const LaserScan& scan);
-		void generateMap(nav_msgs::OccupancyGrid& cur_map);
-		void solve(unsigned int iterations);
-	private:
-		//
-		unsigned int idCounter;
-		double resolution, range_threshold, max_range, min_range;
-		// ScanMatcher matcher;
-		ScanGrid scanToOccGrid(const LaserScan& scan, GraphPose& pose);
-		void addNearbyConstraints(int close_limit, int step_size, double dist_limit, double min_dist_delta, double min_angle_delta);
-		float rot_distance(float theta1, float theta2);
-		float distance(float x1, float x2, float y1, float y2);
-		void createTfFromXYTheta(double x, double y, double theta, tf::Transform& t);
+ public:
+  vector<Node*> node_list;
+  vector<Edge*> edge_list;
+  Node * last_node;
+  //
+  Graph(double resolution, double range_threshold, double max_range, double min_range);
+  ~Graph();
+  void addNode(GraphPose pose, const LaserScan& scan);
+  void generateMap(nav_msgs::OccupancyGrid& cur_map);
+  void solve(unsigned int iterations);
+ 
+ private:
+  unsigned int idCounter;
+  double resolution, range_threshold, max_range, min_range;
+  // ScanMatcher matcher;
+  ScanGrid scanToOccGrid(const LaserScan& scan, GraphPose& pose);
+  void addNearbyConstraints(int close_limit, int step_size, double dist_limit, double min_dist_delta, double min_angle_delta);
+  float rot_distance(float theta1, float theta2);
+  float distance(float x1, float x2, float y1, float y2);
+  void createTfFromXYTheta(double x, double y, double theta, tf::Transform& t);
 };
 #endif
