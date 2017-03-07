0da7ac11096c5ec7db67c3cacad91a966d802bf6
Joan Solà <jsola@iri.upc.edu>
2017-03-07 10:38:08 +0100

    Comments to DS

--- src/odometry/src/odometry.cpp
+++ src/odometry/src/odometry.cpp
@@ -11,7 +11,13 @@
 
 #include <geometry_msgs/Twist.h>
 
-double vx, vy, vth, Delta_x, Delta_y, Delta_th;
+// JS: David, I guess this file is the result of a copypaste from HW1.
+// There are many things not needed, and I would like to use the logic explained in the Drive document
+// Therefore I have put many messages here, with indications.
+// Please feel free to consult me
+
+double vx, vy, vth;
+//, Delta_x, Delta_y, Delta_th; // JS: We do not need Delta
 std::vector<common::Odometry> buffer_odom;
 
 common::Pose2DWithCovariance create_Pose2DWithCovariance_msg(double x, double y, double th, Eigen::MatrixXd m) {
@@ -31,8 +37,9 @@ common::Pose2DWithCovariance create_Pose2DWithCovariance_msg(double x, double y,
 }
 
 // JS: This method is used by more than one node.
-//      It could be placed in package 'common'
-//      and also renamed to 'between(pose1, pose2)'
+//    -  It should be placed in package 'common'
+//    -  and also renamed to 'between(pose1, pose2)'
+//    -  see the document in Drive !!
 common::Pose2DWithCovariance pose_transform(common::Pose2DWithCovariance start_pose,
 					    common::Pose2DWithCovariance end_pose) {
   common::Pose2DWithCovariance transform;
@@ -66,6 +73,8 @@ void add_to_buffer(common::Odometry input) {
   buffer_odom.push_back(input);
 }
 
+// JS: Rename and adjust API to between(keyframe , keyframe) as in the document
+// JS: or, otherwise, just leave as a buffer request, in such case return only one odometry instance, not the increment between two instances.
 bool odometry_buffer_request(common::OdometryBuffer::Request &req, common::OdometryBuffer::Response &res) {
   int buffer_search_position = 0;
   int t_start_buffer_position = 0;
@@ -93,7 +102,7 @@ bool odometry_buffer_request(common::OdometryBuffer::Request &req, common::Odome
   if(t_start_found && t_end_found) {
     common::Pose2DWithCovariance t_start_pose = odometry_buffer_frozen[t_start_buffer_position].pose;
     common::Pose2DWithCovariance t_end_pose = odometry_buffer_frozen[t_end_buffer_position].pose;
-    res.delta = pose_transform(t_start_pose, t_end_pose);
+    res.delta = pose_transform(t_start_pose, t_end_pose); // JS: we want one pose returned, not the pose increment
     return true;
   }
 
@@ -116,9 +125,10 @@ int main(int argc, char** argv) {
   vy = 0.0;
   vth = 0.0;
 
-  Delta_x = 0.0;
-  Delta_y = 0.0;
-  Delta_th = 0.0;
+  // JS: we are not using Delta
+//  Delta_x = 0.0;
+//  Delta_y = 0.0;
+//  Delta_th = 0.0;
 
   while(ros::ok()) {
     current_time = ros::Time::now();
@@ -127,38 +137,58 @@ int main(int argc, char** argv) {
     double delta_y = vy * delta_t;
     double delta_th = vth * delta_t;
 
-    double Delta_th_cos = cos ( Delta_th );
-    double Delta_th_sin = sin ( Delta_th );
+    // JS: we are not using Delta
+//    double Delta_th_cos = cos ( Delta_th );
+//    double Delta_th_sin = sin ( Delta_th );
 
-    Delta_x += ( delta_x * Delta_th_cos ) - ( delta_y * Delta_th_sin );
-    Delta_y += ( delta_y * Delta_th_sin ) + ( delta_y * Delta_th_cos );
-    Delta_th += delta_th;
-    Delta_th = std::fmod( Delta_th + M_PI, 2 * M_PI ) - M_PI;
+    // JS: we are not using Delta
+//    Delta_x += ( delta_x * Delta_th_cos ) - ( delta_y * Delta_th_sin );
+//    Delta_y += ( delta_y * Delta_th_sin ) + ( delta_y * Delta_th_cos );
+//    Delta_th += delta_th;
+//    Delta_th = std::fmod( Delta_th + M_PI, 2 * M_PI ) - M_PI;
 
-    Eigen::Matrix3d J_D_D = Eigen::Matrix3d::Zero(3, 3);
-    J_D_D(0, 0) = 1;
-    J_D_D(0, 2) = ( -1 * delta_x * Delta_th_sin ) - ( delta_y * Delta_th_cos );
-    J_D_D(1, 1) = 1;
-    J_D_D(1, 2) = ( delta_x * Delta_th_cos ) - ( delta_y * Delta_th_sin );
-    J_D_D(2, 2) = 1;
+    // JS: we are not integrating covariances through Jacobians, but through constants k_d_d, k_r_d and k_r_r.
+//    Eigen::Matrix3d J_D_D = Eigen::Matrix3d::Zero(3, 3);
+//    J_D_D(0, 0) = 1;
+//    J_D_D(0, 2) = ( -1 * delta_x * Delta_th_sin ) - ( delta_y * Delta_th_cos );
+//    J_D_D(1, 1) = 1;
+//    J_D_D(1, 2) = ( delta_x * Delta_th_cos ) - ( delta_y * Delta_th_sin );
+//    J_D_D(2, 2) = 1;
 
-    Eigen::Matrix3d J_D_d = Eigen::Matrix3d::Zero(3, 3);
-    J_D_d(0, 0) = Delta_th_cos;
-    J_D_d(0, 1) = -1 * Delta_th_sin;
-    J_D_d(1, 0) = Delta_th_sin;
-    J_D_d(1, 1) = Delta_th_cos;
-    J_D_d(2, 2) = 1;
+    // JS: we are not integrating covariances through Jacobians, but through constants k_d_d, k_r_d and k_r_r.
+//    Eigen::Matrix3d J_D_d = Eigen::Matrix3d::Zero(3, 3);
+//    J_D_d(0, 0) = Delta_th_cos;
+//    J_D_d(0, 1) = -1 * Delta_th_sin;
+//    J_D_d(1, 0) = Delta_th_sin;
+//    J_D_d(1, 1) = Delta_th_cos;
+//    J_D_d(2, 2) = 1;
 
-    common::Odometry delta_odom;
-    delta_odom.ts = current_time;
-    delta_odom.pose = create_Pose2DWithCovariance_msg(delta_x, delta_y, delta_th, J_D_d);
+    // JS: we are not publishing delta
+//    common::Odometry delta_odom;
+//    delta_odom.ts = current_time;
+//    delta_odom.pose = create_Pose2DWithCovariance_msg(delta_x, delta_y, delta_th, J_D_d);
 
-    common::Odometry Delta_odom;
-    Delta_odom.ts = current_time;
-    Delta_odom.pose = create_Pose2DWithCovariance_msg(Delta_x, Delta_y, Delta_th, J_D_D);
+    // JS: we are not using Delta
+//    common::Odometry Delta_odom;
+//    Delta_odom.ts = current_time;
+//    Delta_odom.pose = create_Pose2DWithCovariance_msg(Delta_x, Delta_y, Delta_th, J_D_D);
+
+    // JS: we need to integrate the pose, of type Pose2DWithCovariance:
+    // JS: we need to create a Odometry message with the integrated pose and the timestamp, consult the Drive document
+    // JS: we need to publish the last pose, of type Pose2D, with (x, y, th)
+    // JS: proceed as follows (consult the Drive doc, and use compose() for the integration of 'delta' onto the pose (x,y,th).
+    //   - Integrate pose
+    //   - Compute covariance
+    //   - Construct odometry message
+    //   - Bufferize odometry message
+    //   - Publish Pose2D(x,y,z)
+
+
+    // JS: we are not buffering 'delta'
+//    add_to_buffer(delta_odom);
     
-    add_to_buffer(delta_odom);
-    odom_pub.publish(Delta_odom);
+    //JS: we are not publishing 'Delta'
+//    odom_pub.publish(Delta_odom);
 
     ros::spinOnce();
     loop_rate.sleep();
